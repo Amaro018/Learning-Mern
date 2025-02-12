646756f2b1b892225493f3b1e4c3321d731e2c7d
Amaro018 <gladi0018@gmail.com>
2025-02-12 18:00:29 +0800

    fixing

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -18,6 +18,8 @@ app.use(morgan("dev"));
 
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: false }));
+
 app.use(
     cors({
         origin: "http://localhost:3000", // Allow sending cookies from frontend to backend
