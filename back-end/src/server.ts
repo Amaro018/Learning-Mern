08dd7817dfb33a20566aeb29893bac3f2db39318
Amaro018 <gladi0018@gmail.com>
2025-02-12 20:23:55 +0800

    fixing render deploy

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -3,11 +3,12 @@ import  env from "./util/validateEnv";
 import mongoose from "mongoose";
 
 
-const port = env.PORT;
+const port = env.PORT ;
 
 const mongoUri = env.MONGO_CONNECTION_STRING
 
-mongoose.connect(mongoUri!).then(() => {
+mongoose.connect(mongoUri!,{ ssl: true, tlsAllowInvalidCertificates: true,} ).then(() => {
+    
     console.log("Connected to MongoDB")
     app.listen(port, () => {
         console.log(`Example app listening on port ${port}`);
