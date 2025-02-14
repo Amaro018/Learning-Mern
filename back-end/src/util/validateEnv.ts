8a5f3e9d4cc8c6247b8b050550b31a181ab0e21c
Amaro018 <gladi0018@gmail.com>
2025-02-14 22:34:56 +0800

    add connection string as url

--- back-end/src/util/validateEnv.ts
+++ back-end/src/util/validateEnv.ts
@@ -1,9 +1,9 @@
 import "dotenv/config" 
 import { cleanEnv } from "envalid";
-import { port, str } from "envalid/dist/validators";
+import { port, str, url } from "envalid/dist/validators";
 
 export default cleanEnv(process.env, {
-    MONGO_CONNECTION_STRING: str(),
+    MONGO_CONNECTION_STRING: url(),
     PORT: port(),
     SESSION_SECRET: str()
 });
