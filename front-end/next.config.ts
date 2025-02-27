c7cb4227a84b43120dc5157bf88e9e42652eb284
Amaro018 <gladi0018@gmail.com>
2025-02-27 12:19:47 +0800

    changing path of api

--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -7,8 +7,8 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*", // Intercept calls to /api
-        destination: "http://localhost:5000/api/:path*", // Forward them to backend
-        // destination: "https://learning-mern-chi.vercel.app/api/:path*", // Forward them to backend
+        // destination: "http://localhost:5000/api/:path*", // Forward them to backend
+        destination: "https://learning-mern-chi.vercel.app/api/:path*", // Forward them to backend
       },
     ];
   },
